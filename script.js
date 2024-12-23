0378ac49ce2f6f4be0a02ebbef190592e9763cc0
rittyon715 <se2020na2023@gmail.com>
2024-12-23 23:27:46 +0900

    修正３

--- script.js
+++ script.js
@@ -1,33 +1,18 @@
-//スムーススクロール//
-// document.querySelectorAll('nav a').forEach(anchor => {
-//     anchor.addEventListener('click', function(e) {
-//         e.preventDefault();
-
-//         const targetId = this.getAttribute('href').substring(1);
-//         const targetElement = document.getElementById(targetId);
-
-//         window.scrollTo({
-//             top: targetElement.offsetTop,
-//             behavior: 'smooth'
-//         });
-//     });
-// });
-
 $(function(){
-    /*=================================================
-    スムーススクロール
-    ===================================================*/
-    // ページ内のリンクをクリックした時に動作する
-    $('a[href^="#"]').click(function(){
-      // リンクを取得
-      let href= $(this).attr("href");
-      // ジャンプ先のid名をセット
-      let target = $(href == "#" || href == "" ? 'html' : href);
-      // トップからジャンプ先の要素までの距離を取得
-      let position = target.offset().top;
-      // animateでスムーススクロールを行う
-      // 600はスクロール速度で単位はミリ秒
-      $("html, body").animate({scrollTop:position}, 600, "swing");
-      return false;
-    });
-  })
+  /*=================================================
+  スムーススクロール
+  ===================================================*/
+  // ページ内のリンクをクリックした時に動作する
+  $('a[href^="#"]').click(function(){
+    // リンクを取得
+    let href= $(this).attr("href");
+    // ジャンプ先のid名をセット
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    // トップからジャンプ先の要素までの距離を取得
+    let position = target.offset().top;
+    // animateでスムーススクロールを行う
+    // 600はスクロール速度で単位はミリ秒
+    $("html, body").animate({scrollTop:position}, 600, "swing");
+    return false;
+  });
+})
